cebb66dba562fc563a6a9e48cee2b7058b44dd81
anirudh-anil <anirudhkunduthode@gmail.com>
2025-10-12 11:11:08 +0530

    add primes

---
+++ primes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    long int a, b, num, div;
+    int i;
+    printf("Enter the range (inclusive) seperated by a space: ");
+    scanf(" %ld %ld", &a, &b);
+    if (a<0 || b<0 || a>b){
+        printf("Check Input\n");
+        return 0;
+    }
+    for (num=a; num<=b; num++){
+        div = 0;
+        for(i=1; i<=num; i++){
+            if (num % i == 0) div++;
+        }
+        if (div == 2) printf("%ld ", num);
+    }
+    printf("\n");
+
+}
