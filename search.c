8568bb08e5584cb6a039deab9411582b555d191f
anirudh-anil <anirudhkunduthode@gmail.com>
2025-11-09 19:54:34 +0530

    Fix search.c

--- search.c
+++ search.c
@@ -4,7 +4,7 @@ int sSequential(int [], int, int);
 int sBinary(int [], int, int);
 int main()
 {
-    int n, j, key, ss, sb;
+    int n, i, j, key, ss, sb, temp;
     printf("Number of numbers: "); scanf(" %d", &n);
     int numbers[n];
     printf("Enter %d numbers: ", n);
@@ -12,12 +12,23 @@ int main()
     printf("Enter key to search: "); scanf("%d", &key);
 
     ss = sSequential(numbers, n, key);
+    // Sorting for Binary search to work
+    for (i=0; i<n-1; i++)
+        for (j=0; j<n-i-1;j++){
+            if (numbers[j]>numbers[j+1]){
+                temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp;
+            }
+        }
+
     sb = sBinary(numbers, n, key);
     
     printf("Sequential search: ");
-    if (ss != -1) printf("Found at index %d position %d\n", ss, ss+1); else printf("Not found!");
-    printf("Binary search: ");
-    if (sb != -1) printf("Found at index %d position %d\n", sb, sb+1); else printf("Not found!");
+    if (ss != -1) printf("Found at index %d position %d\n", ss, ss+1); else printf("Not found!\n");
+    if (sb!=-1) printf("Binary search has found the key in entered numbers\n"); else printf("Binary search couldn't find the key!\n");
+
+    
 
     return 0;
 }
